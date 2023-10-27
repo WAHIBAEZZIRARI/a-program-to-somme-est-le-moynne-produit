cd8e25265e7791762a409de22b299f3dfae993a7
WAHIBAEZZIRARI <146207125+WAHIBAEZZIRARI@users.noreply.github.com>
2023-10-27 13:52:00 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,31 @@
+def somme (N):
+    for i in range(0,10):
+        s=s+N 
+    return s
+def produit (T):
+    for i in range(0,10):
+        M=M*T(i)
+    return M
+def moynne (L):
+    N=somme(T)/10
+    return N
+def pos_neg(T):
+    print("les element positif est:")
+    
+    for i in range(0,10):
+        if T[i]>0:
+            print(T[i])
+    print("les element negatif est:")
+    for i in range(0,10):
+        if T[i]<0:
+            print(T[i])
+T=[]
+
+for i in range (0,10):
+   M .append(int(input(f"entrerun nombre de la case {i+1}:")))
+
+
+
+
+
+    
